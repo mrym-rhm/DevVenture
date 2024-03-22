9f67ba04498af62590962bc2d4bd96e6a0bb7fdd
Maryam Rahimi <maryam.rahimie94@gmail.com>
2024-03-22 20:18:55 +0330

    feat: all 5 layers added

--- applications/src/parallaxBackgrounds/script.js
+++ applications/src/parallaxBackgrounds/script.js
@@ -3,7 +3,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
-let gameSpeed = 15;
+let gameSpeed = 5;
 
 // new Image() has the same functionality as document.createElement("img")
 const backgroundLayer1 = new Image();
@@ -51,12 +51,20 @@ class Layer {
     }
 }
 
-const layer4 = new Layer(backgroundLayer4, 0.5);
+const layer1 = new Layer(backgroundLayer1, 0.2);
+const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer3 = new Layer(backgroundLayer3, 0.6);
+const layer4 = new Layer(backgroundLayer4, 0.8);
+const layer5 = new Layer(backgroundLayer5, 1);
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    layer4.update();
-    layer4.draw();
+    gameObjects.forEach(object => {
+        object.update();
+        object.draw();
+    })
     requestAnimationFrame(animate);
 }
 
